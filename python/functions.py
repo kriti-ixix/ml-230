6b9b60602ae1bca923ec998b25147f600021fbac
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-17 08:47:11 +0530

    Initial commit

---
+++ python/functions.py
@@ -0,0 +1,21 @@
+def addNumbers():
+    x = 5
+    y = 10
+    print("Sum is", x+y)
+
+def subNumbers(x, y):
+    print("Difference is:", x-y)
+
+'''
+Types of functions:
+Paramters:
+- Default
+- Paramterised
+
+Return: 
+- No return type
+- Return type
+'''
+
+addNumbers()
+subNumbers(10, 5)
---
+++ python/hello.py
@@ -0,0 +1,28 @@
+#print("Hello world")
+
+#This is a single line comment
+
+'''
+Data Types:
+- Integer
+- Float
+- Boolean
+- String  
+'''
+
+# x = 5
+# y = 10
+
+# # print(x + y)
+# # print(x - y)
+# # print(x * y)
+# # print(x / y)
+# # print(x % y)
+
+# print("Hello", end=", ")
+# print("World")
+
+name = input("Enter your name: ")
+marks = float(input("Enter your marks: "))
+
+print(name, marks, sep=": ")
