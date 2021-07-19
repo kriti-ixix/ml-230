57a9e8c277d8060d7da3217b8cbc7da6fea0df16
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-19 20:44:10 +0530

    strings & control flow statements

---
+++ python/strings.py
@@ -0,0 +1,31 @@
+myString = "Hello world!"
+myString2 = "How are you?"
+
+#String Indexing
+print(myString[2])
+print(myString[7])
+print(myString[-1]) #Negative Indexing
+
+#String slicing
+#string[start=0 : stop=len(exclusive) : step=1]
+print(myString[2:8])
+print(myString[2:8:2])
+print(myString[::-1]) #Reverse a string
+
+#Length of a string
+print(len(myString)) 
+
+#String concatenation
+print(myString + " " + myString2)
+
+#String casing
+print(myString.upper())
+print(myString.lower())
+
+#String splitting
+print(myString.split())
+print(myString2.split())
+
+tweet = "Good morning #niceweather #feelinggood #sunny"
+print(tweet.split("#"))
+print(tweet.split("#", 1))
